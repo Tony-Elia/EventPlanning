332f093dddeb924ff74b34140ffeb6d36eadf034
Tony-Elia <antony.safaga@gmail.com>
2025-12-03 12:01:08 +0200

    configure nwidart modules

---
+++ Modules/Service/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nwidart/service',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '01fbe53a301b92544eec96717af5665a04164d61',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nwidart/service' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '01fbe53a301b92544eec96717af5665a04164d61',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
